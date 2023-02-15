02d7a25a4190785904fdd832b5079cde6cfad6b4
Guilherme Martins <guichasemartinsgmr@hotmail.com>
2023-02-15 01:36:05 -0300

    gerar remessa com defaultvalues caso campo nao seja enviado

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,185 +1,185 @@
 class RemessaPadraoConstants {
   static get TIPO_REGISTRO() {
-    return { value: "1", filler: "0", size: 1 };
+    return { value: "1", filler: "0", size: 1, formatStyle: (value, fill, size) => formatStart(value, fill, size) };
   }
 
   static get CODIGO_INSCRICAO() {
-    return { value: "01", filler: "00", size: 2 };
+    return { value: "01", filler: "00", size: 2, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get NUMERO_INSCRICAO() {
-    return { value: "00012044551802", filler: "0000000000000", size: 14 };
+    return { value: "00012044551802", filler: "00000000000000", size: 14, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get AGENCIA() {
-    return { value: "1500", filler: "0000", size: 4 };
+    return { value: "1500", filler: "0000", size: 4, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get ZEROS() {
-    return { value: "00", filler: "00", size: 2 };
+    return { value: "00", filler: "00", size: 2, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get CONTA() {
-    return { value: "05206", filler: "00000", size: 5};
+    return { value: "05206", filler: "00000", size: 5, formatStyle: (value, fill, size) => formatStart(value, fill, size) };
   }
 
   static get DAC() {
-    return { value: "2", filler: "0", size: 1 };
+    return { value: "2", filler: "0", size: 1, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get BRANCOS1() {
-    return { value: Brancos.QUATRO, filler: Brancos.QUATRO, size: 4 };
+    return { value: Brancos.QUATRO, filler: Brancos.QUATRO, size: 4, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get INSTRUCAO_ALEGACAO() {
-    return { value: "0001", filler: "0000", size: 4 };
+    return { value: "0001", filler: "0000", size: 4, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get USO_EMPRESA() {
-    return { value: ("EMPRESA" + gerarNossoNumeroAleatorio() + Brancos.DEZ), filler: Brancos.VINTE_CINCO, size: 25 };
+    return { value: ("EMPRESA" + gerarNossoNumeroAleatorio() + Brancos.DEZ), filler: Brancos.VINTE_CINCO, size: 25, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get NOSSO_NUMERO() {
-    return { value: gerarNossoNumeroAleatorio() , filler: "00000000", size: 8 };
+    return { value: gerarNossoNumeroAleatorio() , filler: "00000000", size: 8, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get MOEDA_VARIAVEL() {
-    return { value: "0000000000199", filler: "0000000000000", size: 13 };
+    return { value: "0000000000199", filler: "0000000000000", size: 13, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get NUMERO_CARTEIRA() {
-    return { value: "109", filler: "000", size: 3 };
+    return { value: "109", filler: "000", size: 3, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get USO_BANCO() {
-    return { value: ("BANCO" + gerarNossoNumeroAleatorio() + Brancos.OITO), filler: Brancos.VINTE_UM, size: 21 };
+    return { value: ("BANCO" + gerarNossoNumeroAleatorio() + Brancos.OITO), filler: Brancos.VINTE_UM, size: 21, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get CARTEIRA() {
-    return { value: "I", filler: Brancos.UM, size: 1 };
+    return { value: "I", filler: Brancos.UM, size: 1, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get CODIGO_OCORRENCIA() {
-    return { value: "01", filler: "00", size: 2 };
+    return { value: "01", filler: "00", size: 2, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get NUMERO_DOCUMENTO() {
-    return { value: "DOC1234567", filler: Brancos.DEZ, size: 10 };
+    return { value: "DOC1234567", filler: Brancos.DEZ, size: 10, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get VENCIMENTO() {
-    return { value: gerarDataVencimento(), filler: "000000", size: 6 };
+    return { value: gerarDataVencimento(), filler: "000000", size: 6, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get VALOR_TITULO() {
-    return { value: "0000000010000", filler: "0000000000000", size: 13 };
+    return { value: "0000000010000", filler: "0000000000000", size: 13, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get CODIGO_BANCO() {
-    return { value: "341", filler: "000", size: 3 };
+    return { value: "341", filler: "000", size: 3, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get AGENCIA_COBRADORA() {
-    return { value: "12345", filler: "00000", size: 5 };
+    return { value: "12345", filler: "00000", size: 5, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get ESPECIE() {
-    return { value: "01", filler: Brancos.DOIS, size: 2 };
+    return { value: "01", filler: Brancos.DOIS, size: 2, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get ACEITE() {
-    return { value: "A", filler: Brancos.UM, size: 1 };
+    return { value: "A", filler: Brancos.UM, size: 1, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get DATA_EMISSAO() {
-    return { value: gerarDataEmissao(), filler:"000000" , size: 6 };
+    return { value: gerarDataEmissao(), filler:"000000" , size: 6, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get INSTRUCAO1() {
-    return { value: "01", filler: Brancos.DOIS, size: 2 };
+    return { value: "01", filler: Brancos.DOIS, size: 2, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get INSTRUCAO2() {
-    return { value: "30", filler: Brancos.DOIS, size: 2 };
+    return { value: "30", filler: Brancos.DOIS, size: 2, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
   static get VALOR_JUROS() {
-    return { value: "0000000000199", filler: "0000000000000", size: 13 };
+    return { value: "0000000000199", filler: "0000000000000", size: 13, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get DATA_DESCONTO() {
-    return { value: gerarDataDesconto(), filler: "000000", size: 6 };
+    return { value: gerarDataDesconto(), filler: "000000", size: 6, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get VALOR_DESCONTO() {
-    return { value: "0000000000199", filler: "0000000000000", size: 13 };
+    return { value: "0000000000199", filler: "0000000000000", size: 13, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get VALOR_IOF() {
-    return { value: "0000000000199", filler: "0000000000000", size: 13 };
+    return { value: "0000000000199", filler: "0000000000000", size: 13, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get ABATIMENTO() {
-    return { value: "0000000000199", filler: "0000000000000", size: 13 };
+    return { value: "0000000000199", filler: "0000000000000", size: 13, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get CODIGO_INSCRICAO_PAGADOR() {
-    return { value: "01", filler: "00", size: 2 };
+    return { value: "01", filler: "00", size: 2, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get NUMERO_INSCRICAO_PAGADOR() {
-    return { value: "00024498951670", filler: "00000000000000", size: 14 };
+    return { value: "00024498951670", filler: "00000000000000", size: 14, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get NOME() {
-    return { value: "Calebe Murilo Novaes" + Brancos.DEZ, filler: "1", size: 1 };
+    return { value: "Calebe Murilo Novaes".padEnd(30,Brancos.UM), filler: Brancos.TRINTA, size: 1, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get BRANCOS2() {
-    return { value: Brancos.DEZ, filler: Brancos.DEZ, size: 10 };
+    return { value: Brancos.DEZ, filler: Brancos.DEZ, size: 10, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get LOGRADOURO() {
-    return { value: "Rua Alfaiate Reis".padEnd(40,Brancos.UM), filler: Brancos.QUARENTA, size: 40 };
+    return { value: "Rua Alfaiate Reis".padEnd(40,Brancos.UM), filler: Brancos.QUARENTA, size: 40, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get BAIRRO() {
-    return { value: "Colonial".padEnd(12,Brancos.UM), filler: Brancos.DOZE, size: 12 };
+    return { value: "Colonial".padEnd(12,Brancos.UM), filler: Brancos.DOZE, size: 12, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get CEP() {
-    return { value: "93212050", filler: Brancos.OITO, size: 8 };
+    return { value: "93212050", filler: "00000000", size: 8, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get CIDADE() {
-    return { value: "Sapucaia do Sul".padEnd(15,Brancos.UM), filler: Brancos.QUINZE, size: 15 };
+    return { value: "Sapucaia do Sul".padEnd(15,Brancos.UM), filler: Brancos.QUINZE, size: 15, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get ESTADO() {
-    return { value: "RS", filler: Brancos.DOIS, size: 2 };
+    return { value: "RS", filler: Brancos.DOIS, size: 2, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get SACADOR_AVALISTA() {
-    return { value: "Vera e Luis Marketing Ltda".padEnd(30,Brancos.UM), filler: Brancos.TRINTA, size: 30 };
+    return { value: "Vera e Luis Marketing Ltda".padEnd(30,Brancos.UM), filler: Brancos.TRINTA, size: 30, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get BRANCOS3() {
-    return { value: Brancos.QUATRO, filler: Brancos.QUATRO, size: 4 };
+    return { value: Brancos.QUATRO, filler: Brancos.QUATRO, size: 4, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get DATA_MORA() {
-    return { value: gerarDataMora(), filler: "000000", size: 6 };
+    return { value: gerarDataMora(), filler: "000000", size: 6, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get PRAZO() {
-    return { value: "05", filler: "00", size: 5 };
+    return { value: "05", filler: "00", size: 2, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 
   static get BRANCOS4() {
-    return { value: Brancos.UM, filler: Brancos.UM, size: 1 };
+    return { value: Brancos.UM, filler: Brancos.UM, size: 1, formatStyle: (value, fill, size) => formatEnd(value, fill, size) };
   }
 
   static get NUMERO_SEQUENCIAL() {
-    return { value: "000002", filler: "000000", size: 6 };
+    return { value: "000002", filler: "000000", size: 6, formatStyle: (value, fill, size) => formatStart(value, fill, size)  };
   }
 }
 
--- scripts/fieldUtils.js
+++ scripts/fieldUtils.js
@@ -1,7 +1,17 @@
-function formateStart(str,size,filler){
+function formatStart(str,size,filler){
     return str.lenght > size ? str.slice(0,size) : str.padStart(size,filler)
 }
 
-function formateEnd(str,size,filler){
+function formatEnd(str,size,filler){
     return str.lenght > size ? str.trim(0,size) : str.padEnd(size,filler)
 }
+
+function getValueOrFiller(field, configuration){
+    var fieldValue = field.value;
+    var fillerValue = configuration.filler;
+    if(fieldValue == "" || fieldValue == "undefined"){
+        return fillerValue;
+    }
+    var size = configuration.size;
+    return configuration.formatStyle(fieldValue,size,fillerValue[0]);
+} 
