6800632a0711aa3839d86e113c7aabb915fbd3d1
Guilherme Martins <guichasemartinsgmr@hotmail.com>
2023-03-02 01:29:11 -0300

    separado campos configurações e padroes

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -235,24 +235,12 @@ function lockUnlockButton(fieldId, button) {
   }
 
   function salvarConfiguracoes() {
-    var agencia = document.getElementById("agenciaPadrao").value;
-    var conta = document.getElementById("contaPadrao").value;
-    var dac = document.getElementById("dacPadrao").value;
     var headerArquivoPadrao = document.getElementById("headerArquivoPadrao").value;
-    var nossoNumero = document.getElementById("nossoNumeroPadrao").value;
-    var codigoOcorrenciaPadrao = document.getElementById("codigoOcorrenciaPadrao").value;
     var complementoArquivoPadrao = document.getElementById("complementoArquivoPadrao").value;
     var isComplementoArquivoPadrao = document.getElementById("isComplementoArquivoPadrao").checked;
     var complemento = document.getElementById("complementoLinhaPadrao").value;
     var isComplemento = document.getElementById("isComplementoPadrao").checked;
-    var isSobreporLinha = document.getElementById("isSobreporLinha").checked;
     var fillerPadrao = document.getElementById("fillerPadrao").value;
-
-    localStorage.setItem("agenciaPadrao", agencia);
-
-    localStorage.setItem("contaPadrao", conta);
-
-    localStorage.setItem("dacPadrao", dac);
 
     localStorage.setItem("headerArquivoPadrao", headerArquivoPadrao);
 
@@ -264,12 +252,6 @@ function lockUnlockButton(fieldId, button) {
 
     localStorage.setItem("isComplementoPadrao", isComplemento);
 
-    localStorage.setItem("nossoNumeroPadrao", nossoNumero);
-
-    localStorage.setItem("codigoOcorrenciaPadrao", codigoOcorrenciaPadrao);
-
-    localStorage.setItem("isSobreporLinha", isSobreporLinha);
-
     localStorage.setItem("fillerPadrao", fillerPadrao);
 
     var message = "Configurações Salvas!";
@@ -277,22 +259,63 @@ function lockUnlockButton(fieldId, button) {
   }
   
   function apagarConfiguracoes(){
-    localStorage.clear();
-    document.getElementById("agenciaPadrao").value = "";
-    document.getElementById("contaPadrao").value = "";
-    document.getElementById("dacPadrao").value = "";
+    localStorage.removeItem('headerArquivoPadrao');
+    localStorage.removeItem('complementoArquivoPadrao');
+    localStorage.removeItem('isComplementoArquivoPadrao');
+    localStorage.removeItem('complementoLinhaPadrao');
+    localStorage.removeItem('isComplementoPadrao');
+    localStorage.removeItem('fillerPadrao');
+
     document.getElementById("headerArquivoPadrao").value = "";
-    document.getElementById("nossoNumeroPadrao").value = "";
     document.getElementById("complementoArquivoPadrao").value = "";
     document.getElementById("isComplementoArquivoPadrao").checked = false;
     document.getElementById("complementoLinhaPadrao").value = "";
     document.getElementById("isComplementoPadrao").checked = false;
-    document.getElementById("isSobreporLinha").checked = false;
-    document.getElementById("codigoOcorrenciaPadrao").value = "";
     document.getElementById("fillerPadrao").value = "padrao";
     alert("Configurações Deletadas")
   }
 
+
+  function salvarCamposPadrao() {
+    var agencia = document.getElementById("agenciaPadrao").value;
+    var conta = document.getElementById("contaPadrao").value;
+    var dac = document.getElementById("dacPadrao").value;
+    var nossoNumero = document.getElementById("nossoNumeroPadrao").value;
+    var codigoOcorrenciaPadrao = document.getElementById("codigoOcorrenciaPadrao").value;
+    var isSobreporLinha = document.getElementById("isSobreporLinha").checked;
+
+    localStorage.setItem("agenciaPadrao", agencia);
+
+    localStorage.setItem("contaPadrao", conta);
+
+    localStorage.setItem("dacPadrao", dac);
+
+    localStorage.setItem("nossoNumeroPadrao", nossoNumero);
+
+    localStorage.setItem("codigoOcorrenciaPadrao", codigoOcorrenciaPadrao);
+
+    localStorage.setItem("isSobreporLinha", isSobreporLinha);
+
+    var message = "Campos Padrões Salvos!";
+    alert(message);
+  }
+
+  function apagarCamposPadrao(){
+    localStorage.removeItem('agenciaPadrao');
+    localStorage.removeItem('contaPadrao');
+    localStorage.removeItem('dacPadrao');
+    localStorage.removeItem('nossoNumeroPadrao');
+    localStorage.removeItem('isSobreporLinha');
+    localStorage.removeItem('codigoOcorrenciaPadrao');
+
+    document.getElementById("agenciaPadrao").value = "";
+    document.getElementById("contaPadrao").value = "";
+    document.getElementById("dacPadrao").value = "";
+    document.getElementById("nossoNumeroPadrao").value = "";
+    document.getElementById("isSobreporLinha").checked = false;
+    document.getElementById("codigoOcorrenciaPadrao").value = "";
+    alert("Campos Padrões Deletados")
+  }
 
   function alterarModoInstrucao(){
     setModoInstrucao();
