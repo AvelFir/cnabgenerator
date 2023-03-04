0fd06156c84b42e022fe21c43aec0bdbd5032d5b
Guilherme Martins <guichasemartinsgmr@hotmail.com>
2023-03-04 01:43:26 -0300

    adicionado limpar no rateio

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -362,3 +362,13 @@ function lockUnlockButton(fieldId, button) {
   function alterarModoRemessaCompleta(){
     setModoRemessaCompleta();
   }
+
+  function limparInputs(camposDiv) {
+    var inputs = document.querySelectorAll("#" + camposDiv +  " div input");
+    if(inputs.length == 0){
+      inputs = document.querySelectorAll("#" + camposDiv +  " input");
+    }
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].value = '';
+    }
+  }
